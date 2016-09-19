7a4ea48745596eb39b555ccd3853540222e2793a
Sid Khot <sidkhot@cumulusnetworks.com>
2016-09-19 15:52:51 -0700

    bgpd: Fix for CM-12914 EVPN: Need multipath implementation
    Ticket: CM-12914
    Reviewed By: CCR-5194
    Testing Done: Manual

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -14994,6 +14994,9 @@ bgp_vty_init (void)
   install_element (BGP_IPV6_NODE, &bgp_maxpaths_cmd);
   install_element (BGP_IPV6_NODE, &no_bgp_maxpaths_cmd);
   install_element (BGP_IPV6_NODE, &no_bgp_maxpaths_arg_cmd);
+  install_element (BGP_EVPN_NODE, &bgp_maxpaths_cmd);
+  install_element (BGP_EVPN_NODE, &no_bgp_maxpaths_cmd);
+  install_element (BGP_EVPN_NODE, &no_bgp_maxpaths_arg_cmd);
   install_element (BGP_NODE, &bgp_maxpaths_ibgp_cmd);
   install_element(BGP_NODE, &bgp_maxpaths_ibgp_cluster_cmd);
   install_element (BGP_NODE, &no_bgp_maxpaths_ibgp_cmd);
@@ -15009,6 +15012,11 @@ bgp_vty_init (void)
   install_element (BGP_IPV6_NODE, &no_bgp_maxpaths_ibgp_cmd);
   install_element (BGP_IPV6_NODE, &no_bgp_maxpaths_ibgp_arg_cmd);
   install_element (BGP_IPV6_NODE, &no_bgp_maxpaths_ibgp_cluster_cmd);
+  install_element (BGP_EVPN_NODE, &bgp_maxpaths_ibgp_cmd);
+  install_element (BGP_EVPN_NODE, &bgp_maxpaths_ibgp_cluster_cmd);
+  install_element (BGP_EVPN_NODE, &no_bgp_maxpaths_ibgp_cmd);
+  install_element (BGP_EVPN_NODE, &no_bgp_maxpaths_ibgp_cluster_cmd);
+  install_element (BGP_EVPN_NODE, &no_bgp_maxpaths_ibgp_arg_cmd);
 
   /* "timers bgp" commands. */
   install_element (BGP_NODE, &bgp_timers_cmd);
