7eaebef6de25bd6107a243379644ce2247edf633
vivek <vivek@cumulusnetworks.com>
2016-10-17 16:55:37 -0700

    lib: VLAN definition
    Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>
    Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>
    
    Ticket: CM-13204
    Reviewed By: CCR-5256
    Testing Done: None (trivial)

---
+++ lib/vlan.h
@@ -0,0 +1,29 @@
+/* VLAN (802.1q) common header.
+ * Copyright (C) 2016 Cumulus Networks, Inc.
+ *
+ * This file is part of Quagga.
+ *
+ * Quagga is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * Quagga is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GN5U General Public License
+ * along with Quagga; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef _VLAN_H
+#define _VLAN_H
+
+/* VLAN Identifier */
+typedef u_int16_t vlanid_t;
+#define VLANID_MAX 4095
+
+#endif /* _VLAN_H */
