81b349dfd1673c2b0ca59b633589aa2dfe19ae6d
Donald Sharp <sharpd@cumulusnetworks.com>
2016-11-11 18:20:57 -0500

    bgpd: Clean up thread code in bgp_fsm.h
    The BGP thread macro's checked to see if the
    thread is valid, then the thread.h macro's do the
    exact same thing.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgp_fsm.h
+++ bgpd/bgp_fsm.h
@@ -25,55 +25,52 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 /* Macro for BGP read, write and timer thread.  */
 #define BGP_READ_ON(T,F,V)			\
   do {						\
-    if (!(T) && (peer->status != Deleted))	\
+    if (peer->status != Deleted)		\
       THREAD_READ_ON(bm->master,T,F,peer,V);	\
   } while (0)
 
 #define BGP_READ_OFF(T)				\
   do {						\
-    if (T)					\
-      THREAD_READ_OFF(T);			\
+    THREAD_READ_OFF(T);				\
   } while (0)
 
-#define BGP_WRITE_ON(T,F,V)			\
-  do {						\
-    if (!(T) && (peer->status != Deleted))	\
+#define BGP_WRITE_ON(T,F,V)			    \
+  do {						    \
+    if (peer->status != Deleted)		    \
       THREAD_WRITE_ON(bm->master,(T),(F),peer,(V)); \
   } while (0)
 
 #define BGP_PEER_WRITE_ON(T,F,V, peer)			\
   do {							\
-    if (!(T) && ((peer)->status != Deleted))		\
+    if ((peer)->status != Deleted)			\
       THREAD_WRITE_ON(bm->master,(T),(F),(peer),(V));	\
   } while (0)
 
 #define BGP_WRITE_OFF(T)			\
   do {						\
-    if (T)					\
       THREAD_WRITE_OFF(T);			\
   } while (0)
 
-#define BGP_TIMER_ON(T,F,V)			\
-  do {						\
-    if (!(T) && (peer->status != Deleted))	\
+#define BGP_TIMER_ON(T,F,V)			    \
+  do {						    \
+    if (peer->status != Deleted)		    \
       THREAD_TIMER_ON(bm->master,(T),(F),peer,(V)); \
   } while (0)
 
 #define BGP_TIMER_OFF(T)			\
   do {						\
-    if (T)					\
-      THREAD_TIMER_OFF(T);			\
+    THREAD_TIMER_OFF(T);			\
   } while (0)
 
-#define BGP_EVENT_ADD(P,E)			\
-  do {						\
-    if ((P)->status != Deleted)			\
+#define BGP_EVENT_ADD(P,E)				  \
+  do {							  \
+    if ((P)->status != Deleted)				  \
       thread_add_event (bm->master, bgp_event, (P), (E)); \
   } while (0)
 
-#define BGP_EVENT_FLUSH(P)			\
-  do { 						\
-    assert (peer); 				\
+#define BGP_EVENT_FLUSH(P)				\
+  do {							\
+    assert (peer);					\
     thread_cancel_event (bm->master, (P)); 		\
   } while (0)
 
