vincentbernat/quagga

commit 9c8961d521ebdceac546539e1f8ee89e63f33f19

zebra: Fix warning message from kernel
When running zebra we were getting this message:

Nov 11 11:18:47 robot.cumulusnetworks.com kernel: netlink: 8 bytes leftover after parsing attributes in process `zebra'.

This was tracked down to this commit:
e078e5dc8e8b5c80548352db3b64f7014571c390

Modify the request code to know if it should handle the filter or not
as appropriate.

Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

zebra/kernel_netlink.c
@@ -699,6 +699,8 @@ netlink_request (int family, int type, struct nlsock *nl,
   int ret;
   struct sockaddr_nl snl;
   int save_errno;
+  size_t size;
+  void *ptr = NULL;
 
   struct
   {
@@ -706,6 +708,12 @@ netlink_request (int family, int type, struct nlsock *nl,
     struct ifinfomsg ifm;
     struct rtattr ext_req;
     u_int32_t ext_filter_mask;
+  } reqfilter;
+
+  struct
+  {
+    struct nlmsghdr nlh;
+    struct rtgenmsg g;
   } req;
 
   /* Check netlink socket. */
@@ -719,18 +727,33 @@ netlink_request (int family, int type, struct nlsock *nl,
   snl.nl_family = AF_NETLINK;
 
   memset (&req, 0, sizeof req);
-  req.nlh.nlmsg_len = sizeof req;
-  req.nlh.nlmsg_type = type;
-  req.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
-  req.nlh.nlmsg_pid = nl->snl.nl_pid;
-  req.nlh.nlmsg_seq = ++nl->seq;
-  req.ifm.ifi_family = family;
 
-  if (filter_mask)
+  if (!filter_mask)
     {
-      req.ext_req.rta_type = IFLA_EXT_MASK;
-      req.ext_req.rta_len = RTA_LENGTH(sizeof(__u32));
-      req.ext_filter_mask = filter_mask;
+      memset (&req, 0, sizeof req);
+      req.nlh.nlmsg_len = sizeof req;
+      req.nlh.nlmsg_type = type;
+      req.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
+      req.nlh.nlmsg_pid = nl->snl.nl_pid;
+      req.nlh.nlmsg_seq = ++nl->seq;
+      req.g.rtgen_family = family;
+      size = sizeof req;
+      ptr = &req;
+    }
+  else
+    {
+      memset (&reqfilter, 0, sizeof reqfilter);
+      reqfilter.nlh.nlmsg_len = sizeof req;
+      reqfilter.nlh.nlmsg_type = type;
+      reqfilter.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
+      reqfilter.nlh.nlmsg_pid = nl->snl.nl_pid;
+      reqfilter.nlh.nlmsg_seq = ++nl->seq;
+      reqfilter.ifm.ifi_family = family;
+      reqfilter.ext_req.rta_type = IFLA_EXT_MASK;
+      reqfilter.ext_req.rta_len = RTA_LENGTH(sizeof(__u32));
+      reqfilter.ext_filter_mask = filter_mask;
+      size = sizeof reqfilter;
+      ptr = &reqfilter;
     }
 
   /* linux appears to check capabilities on every message
@@ -742,7 +765,8 @@ netlink_request (int family, int type, struct nlsock *nl,
       return -1;
     }
 
-  ret = sendto (nl->sock, (void *) &req, sizeof req, 0,
+
+  ret = sendto (nl->sock, ptr, size, 0,
 		(struct sockaddr *) &snl, sizeof snl);
   save_errno = errno;
 

commit b290dae924b5d1ff47671d67f7f1e739028e390e

zebra: Ensure correct length in netlink message
When a filter mask is passed to the kernel, there was an error in the
length passed, fixed with this commit.

Fixes: 9c8961d521ebdceac546539e1f8ee89e63f33f19

Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

zebra/kernel_netlink.c
@@ -753,7 +753,7 @@ netlink_request (int family, int type, struct nlsock *nl,
   else
     {
       memset (&reqfilter, 0, sizeof reqfilter);
-      reqfilter.nlh.nlmsg_len = sizeof req;
+      reqfilter.nlh.nlmsg_len = sizeof reqfilter;
       reqfilter.nlh.nlmsg_type = type;
       reqfilter.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
       reqfilter.nlh.nlmsg_pid = nl->snl.nl_pid;