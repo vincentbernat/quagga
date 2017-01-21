eea7ed87048f5f290c22fc1f579fe03a2fcf1a95
Donald Sharp <sharpd@cumulusnetworks.com>
2017-01-21 04:54:10 -0500

    pimd: Handle assignment of vif index better
    PIM was handling vif creation deletion poorly
    for interface down and up events.  Fix this
    issue by keeping track of which vif index'es
    we have issued and allow the wholes to be
    filled in.
    
    Ticket: CM-14556
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/pimd.h
+++ pimd/pimd.h
@@ -100,7 +100,6 @@ extern struct thread_master     *master;
 uint32_t                  qpim_debugs;
 int                       qpim_mroute_socket_fd;
 int64_t                   qpim_mroute_socket_creation; /* timestamp of creation */
-int                       qpim_mroute_oif_highest_vif_index;
 struct in_addr            qpim_all_pim_routers_addr;
 int                       qpim_t_periodic; /* Period between Join/Prune Messages */
 struct zclient           *qpim_zclient_update;
