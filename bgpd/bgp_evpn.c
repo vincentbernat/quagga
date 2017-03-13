vincentbernat/quagga

commit 196761913d66d75d6d1a3687948feacfded2818b

bgpd: Allow type-2 routes with IP address and second VNI
The EVPN type-2 (MAC-IP route) can carry MAC address plus IP address
and either 1 or 2 MPLS labels (VNIs). Ensure that though the current
implementation does not do anything with received IP address and
second VNI (if any), it does not treat their presence as an error and
bring down the BGP session.

Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

Ticket: CM-15097
Reviewed By:
Testing Done:

bgpd/bgp_evpn.c
@@ -1728,13 +1728,14 @@ process_type2_route (struct peer *peer, afi_t afi, safi_t safi,
   u_char *tagpnt;
   int ret;
 
-  /* Type-2 route should be either 33, 37 or 49 bytes:
+  /* Type-2 route should be either 33, 37 or 49 bytes or an
+   * additional 3 bytes if there is a second label (VNI):
    * RD (8), ESI (10), Eth Tag (4), MAC Addr Len (1),
    * MAC Addr (6), IP len (1), IP (0, 4 or 16),
    * MPLS Lbl1 (3), MPLS Lbl2 (0 or 3)
-   * TODO: We currently expect only 1 label/tag.
    */
-  if (psize != 33 && psize != 37 && psize != 49)
+  if (psize != 33 && psize != 37 && psize != 49 &&
+      psize != 36 && psize != 40 && psize != 52)
     {
       zlog_err ("%u:%s - Rx EVPN Type-2 NLRI with invalid length %d",
                 peer->bgp->vrf_id, peer->host, psize);
@@ -1780,7 +1781,7 @@ process_type2_route (struct peer *peer, afi_t afi, safi_t safi,
   ipaddr_len = *pfx++;
   if (ipaddr_len == 0)
     p.prefix.ipa_type = IP_ADDR_NONE;
-  else
+  else if (ipaddr_len != IPV4_MAX_BITLEN && ipaddr_len != IPV6_MAX_BITLEN)
     {
       zlog_err ("%u:%s - Rx EVPN Type-2 NLRI with unsupported IP address length %d",
                 peer->bgp->vrf_id, peer->host, ipaddr_len);
@@ -1790,6 +1791,7 @@ process_type2_route (struct peer *peer, afi_t afi, safi_t safi,
   pfx += ipaddr_len;
 
   /* Get the VNI (in MPLS label field). */
+  /* Note: We ignore the second VNI, if any. */
   tagpnt = pfx;
 
   /* Process the route. */

commit cafdc5ae5591e85058aaf2d543bbdbb9eed72c65

bgpd: Correctly handle IP addresses in type-2 route
The IP address length is in bits, ensure the increment of bytes is
done correctly.

Fixes: 196761913d66d75d6d1a3687948feacfded2818b
Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>
Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>

Ticket: CM-15097
Reviewed By: CCR-5754
Testing Done: Manual

bgpd/bgp_evpn.c
@@ -1788,7 +1788,7 @@ process_type2_route (struct peer *peer, afi_t afi, safi_t safi,
       return -1;
     }
 
-  pfx += ipaddr_len;
+  pfx += ipaddr_len / 8;
 
   /* Get the VNI (in MPLS label field). */
   /* Note: We ignore the second VNI, if any. */