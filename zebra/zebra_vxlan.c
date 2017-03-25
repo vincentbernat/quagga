vincentbernat/quagga

commit 0ed8836d2d42bf7f381b80bf30ec15798a385e4c

zebra: Neighbor table definition
Define neighbor table (ARP cache) data structure. This is currently
maintained only for EVPN purposes, hence the entries are stored as
part of the VNI hash table (similar to MAC table).

Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>
Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>

Ticket: CM-14921
Reviewed By: CCR-5888
Testing Done: None

zebra/zebra_vxlan.c
@@ -47,11 +47,13 @@
 DEFINE_MTYPE_STATIC(ZEBRA, ZVNI,      "VNI hash");
 DEFINE_MTYPE_STATIC(ZEBRA, ZVNI_VTEP, "VNI remote VTEP");
 DEFINE_MTYPE_STATIC(ZEBRA, MAC,       "VNI MAC");
+DEFINE_MTYPE_STATIC(ZEBRA, NEIGH,     "VNI Neighbor");
 
 /* definitions */
 typedef struct zebra_vni_t_ zebra_vni_t;
 typedef struct zebra_vtep_t_ zebra_vtep_t;
 typedef struct zebra_mac_t_ zebra_mac_t;
+typedef struct zebra_neigh_t_ zebra_neigh_t;
 
 /*
  * VTEP info
@@ -90,8 +92,11 @@ struct zebra_vni_t_
   /* Local IP */
   struct in_addr local_vtep_ip;
 
-  /* List of local or remote MAC/IP */
+  /* List of local or remote MAC */
   struct hash *mac_table;
+
+  /* List of local or remote neighbors (MAC+IP) */
+  struct hash *neigh_table;
 };
 
 /*
@@ -149,6 +154,36 @@ struct mac_walk_ctx
 
   struct vty *vty;            /* Used by VTY handlers */
   u_int32_t  count;           /* Used by VTY handlers */
+};
+
+/*
+ * Neighbor hash table.
+ *
+ * This table contains the neighbors (IP to MAC bindings) pertaining to
+ * this VNI. This includes local neighbors learnt on the attached VLAN
+ * device that maps to this VNI as well as remote neighbors learnt and
+ * installed by BGP.
+ * Local neighbors will be known against the VLAN device (SVI); however,
+ * it is sufficient for zebra to maintain against the VNI. The correct
+ * VNI will be obtained as zebra maintains the mapping (of VLAN to VNI).
+ */
+struct zebra_neigh_t_
+{
+  /* IP address. */
+  struct ipaddr   ip;
+
+  /* MAC address. */
+  struct ethaddr  emac;
+
+  /* Underlying interface. */
+  ifindex_t ifindex;
+
+  u_int32_t       flags;
+#define ZEBRA_NEIGH_LOCAL   0x01
+#define ZEBRA_NEIGH_REMOTE  0x02
+
+  /* Remote VTEP IP - applicable only for remote neighbors. */
+  struct in_addr r_vtep_ip;
 };
 
 

commit 9f072d2758e7cb42b903a079f2451adc19d60f76

zebra: Fix incorrect reference
An incorrect type was referenced while handling neighbor information.
Fixed.

Fixes: 0ed8836d2d42bf7f381b80bf30ec15798a385e4c
Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

zebra/zebra_vxlan.c
@@ -2340,7 +2340,8 @@ int zebra_vxlan_update_access_vlan (struct interface *ifp,
   struct zebra_l2if_vxlan *zl2if;
   zebra_vni_t *zvni;
   vni_t vni;
-  struct mac_walk_ctx wctx;
+  struct mac_walk_ctx m_wctx;
+  struct neigh_walk_ctx n_wctx;
   struct interface *vlan_if;
 
   zif = ifp->info;
@@ -2399,14 +2400,14 @@ int zebra_vxlan_update_access_vlan (struct interface *ifp,
     neigh_read_for_vlan (zvrf->zns, vlan_if);
 
   /* Reinstall any remote MACs for this VNI - with new VLAN info. */
-  memset (&wctx, 0, sizeof (struct mac_walk_ctx));
-  wctx.zvni = zvni;
-  hash_iterate(zvni->mac_table, zvni_install_mac_hash, &wctx);
+  memset (&m_wctx, 0, sizeof (struct mac_walk_ctx));
+  m_wctx.zvni = zvni;
+  hash_iterate(zvni->mac_table, zvni_install_mac_hash, &m_wctx);
 
   /* Reinstall any remote neighbors for this VNI - with new VLAN info. */
-  memset (&wctx, 0, sizeof (struct neigh_walk_ctx));
-  wctx.zvni = zvni;
-  hash_iterate(zvni->neigh_table, zvni_install_neigh_hash, &wctx);
+  memset (&n_wctx, 0, sizeof (struct neigh_walk_ctx));
+  n_wctx.zvni = zvni;
+  hash_iterate(zvni->neigh_table, zvni_install_neigh_hash, &n_wctx);
 
   return 0;
 }